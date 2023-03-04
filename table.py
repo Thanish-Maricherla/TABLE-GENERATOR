60061c55c9e1efc5832d1ce9db354df24ef3c2dc
Thanish-Maricherla <123317095+Thanish-Maricherla@users.noreply.github.com>
2023-03-04 21:49:15 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,4 @@
+print("Hello, I am Table Generator")
+t = int(input("Enter any number"))
+for i in range(1,11):
+    print(t,"x", i, "=",i*t)
---
+++ while.py
@@ -0,0 +1,7 @@
+print("Hello, I am Table Generator")
+t = int(input("Enter any number"))
+count = 1
+while count <=10:
+
+    print(t,"x",count,"=",t*count)
+    count +=1
